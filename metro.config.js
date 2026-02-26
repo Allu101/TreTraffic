2ac125d162fff6d1ce859129e59c208a8cc00577
Allu101 <a.lahtinen101@gmail.com>
2026-02-26 22:32:21 +0200

    Delete .android folder and metro.config

--- metro.config.js
+++
@@ -1,8 +0,0 @@
-const { getDefaultConfig } = require('expo/metro-config');
-
-const config = getDefaultConfig(__dirname);
-
-// Prefer browser/react-native fields so Metro doesn't pick Node-specific builds
-config.resolver.resolverMainFields = ['react-native', 'browser', 'main'];
-
-module.exports = config;
