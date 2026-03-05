95e88bff2b8fdfecf274fdb00928180991b32c9e
Allu101 <a.lahtinen101@gmail.com>
2026-03-06 00:03:16 +0200

    Remove possible duplicate base_url and add name parameter to error handling function

--- utils/http-requests.js
+++ utils/http-requests.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import { API_KEY } from '@env';
 
-//const BASE_URL = "http://192.168.0.3/api/";
-const BASE_URL = "http://10.203.105.197:5000/api/";
+//const BASE_URL = "http://192.168.0.3:5000/api/";
+const BASE_URL = "https://12f4-2001-99a-19d-2900-5d23-645a-259d-2b80.ngrok-free.app/api/";
 
 const api = axios.create({
   baseURL: BASE_URL,
@@ -13,18 +13,20 @@ const api = axios.create({
   timeout: 3000,
 });
 
-function handleApiError(error) {
-  console.error("API Error:", error);
-  return {
+function handleApiError(error, name) {
+  const errorRes = {
     error: true,
     message: error.response?.data?.message || error.message || "Unknown error",
     status: error.response?.status || null,
+    name: name,
   };
+  console.log("API Error:", errorRes);
+  return errorRes;
 }
 
 async function getAllIntersectionLocations() {
   try {
-    const response = await api.get(`${BASE_URL}locations/intersections/`);
+    const response = await api.get(`locations/intersections/`);
     return response.data || [];
   } catch (error) {
     return handleApiError(error);
@@ -33,32 +35,32 @@ async function getAllIntersectionLocations() {
 
 async function getAllTriggerLines() {
   try {
-    const response = await api.get(`${BASE_URL}triggerlines/`);
+    const response = await api.get(`triggerlines/`);
     return response.data || [];
   } catch (error) {
-    return handleApiError(error);
+    return handleApiError(error, "triggerlines");
   }
 }
 
 async function getIntersectionData(intersection_nro) {
   try {
     const time = new Date().getTime();
-    const response = await api.get(`${BASE_URL}intersections/intersection/${intersection_nro}`);
+    const response = await api.get(`intersections/intersection/${intersection_nro}`);
     console.log(new Date().getTime() - time + ' ms i');
     return response.data || {};
   } catch (error) {
-    return handleApiError(error);
+    return handleApiError(error, "intersection data");
   }
 }
 
 async function getLightGroupsData(lightGroups) {
   try {
     const time = new Date().getTime();
-    const response = await api.get(`${BASE_URL}intersections/lightgroups/${lightGroups}`);
+    const response = await api.get(`intersections/lightgroups/${lightGroups}`);
     console.log(new Date().getTime() - time + ' ms l');
     return response.data || {};
   } catch (error) {
-    return handleApiError(error);
+    return handleApiError(error, "lightgroups data");
   }
 }
 
