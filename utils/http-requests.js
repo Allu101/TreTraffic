28ce95a052eacdb5b7e95ef6990b5c630c7f371c
Allu101 <a.lahtinen101@gmail.com>
2026-06-03 13:27:34 +0300

    Add sign out button and minor timer refactor

--- utils/http-requests.js
+++ utils/http-requests.js
@@ -1,13 +1,8 @@
 import axios from "axios";
-import { API_KEY, GOOGLE_WEB_CLIENT_ID } from '@env';
-import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import { API_KEY } from '@env';
 
 const DEFAULT_BASE_URL = "http://192.168.0.3:5000/api/";
 //const DEFAULT_BASE_URL = "https://1b4b-2001-99a-19d-2900-147-4d20-ec3c-130.ngrok-free.app/api/";
-
-GoogleSignin.configure({
-  webClientId: GOOGLE_WEB_CLIENT_ID,
-});
 
 const etagCache = new Map();
 const dataCache = new Map();
@@ -126,7 +121,7 @@ async function getLightGroupsData(lightGroups, currentMode) {
   }
 }
 
-async function handleGoogleLogin() {
+async function handleGoogleLogin(GoogleSignin) {
   try {
     await GoogleSignin.hasPlayServices();
     const userInfo = await GoogleSignin.signIn();
